59b1cc9d2115c4d9d1da6f44e40611d92fa1d9aa
Wilson Francisca Cheyachi <francawilson98@gmail.com>
2023-10-29 10:37:57 -0700

    Add seperate modules to refactor code

--- bikeshare.py
+++ bikeshare.py
@@ -1,234 +1,93 @@
-## TODO: import all necessary packages and functions
+import time
 
+class BikeShareStatistics:
+    def __init__(self):
+        self.city = None
+        self.time_period = None
 
-## Filenames
-chicago = 'chicago.csv'
-new_york_city = 'new_york_city.csv'
-washington = 'washington.csv'
+    def get_city(self):
+        city = input('\nHello! Let\'s explore some US bikeshare data!\n'
+                     'Would you like to see data for Chicago, New York, or Washington?\n')
+        self.city = city.lower()
 
+    def get_time_period(self):
+        time_period = input('\nWould you like to filter the data by month, day, or not at'
+                            ' all? Type "none" for no time filter.\n')
+        self.time_period = time_period.lower()
 
-def get_city():
-    '''Asks the user for a city and returns the filename for that city's bike share data.
+    def get_month(self):
+        month = input('\nWhich month? January, February, March, April, May, or June?\n')
+        return month.lower()
 
-    Args:
-        none.
-    Returns:
-        (str) Filename for a city's bikeshare data.
-    '''
-    city = input('\nHello! Let\'s explore some US bikeshare data!\n'
-                 'Would you like to see data for Chicago, New York, or Washington?\n')
-    # TODO: handle raw input and complete function
+    def get_day(self, month):
+        day = input('\nWhich day? Please type your response as an integer.\n')
+        return day.lower()
 
+    def popular_month(self, city_file):
+        # TODO: implement popular_month function
 
-def get_time_period():
-    '''Asks the user for a time period and returns the specified filter.
+    def popular_day(self, city_file):
+        # TODO: implement popular_day function
 
-    Args:
-        none.
-    Returns:
-        TODO: fill out return type and description (see get_city for an example)
-    '''
-    time_period = input('\nWould you like to filter the data by month, day, or not at'
-                        ' all? Type "none" for no time filter.\n')
-    # TODO: handle raw input and complete function
+    def popular_hour(self, city_file):
+        # TODO: implement popular_hour function
 
+    def trip_duration(self, city_file):
+        # TODO: implement trip_duration function
 
-def get_month():
-    '''Asks the user for a month and returns the specified month.
+    def popular_stations(self, city_file):
+        # TODO: implement popular_stations function
 
-    Args:
-        none.
-    Returns:
-        TODO: fill out return type and description (see get_city for an example)
-    '''
-    month = input('\nWhich month? January, February, March, April, May, or June?\n')
-    # TODO: handle raw input and complete function
+    def popular_trip(self, city_file):
+        # TODO: implement popular_trip function
 
+    def users(self, city_file):
+        # TODO: implement users function
 
-def get_day(month):
-    '''Asks the user for a day and returns the specified day.
+    def gender(self, city_file):
+        # TODO: implement gender function
 
-    Args:
-        none.
-    Returns:
-        TODO: fill out return type and description (see get_city for an example)
-    '''
-    day = input('\nWhich day? Please type your response as an integer.\n')
-    # TODO: handle raw input and complete function
+    def birth_years(self, city_file):
+        # TODO: implement birth_years function
 
+    def display_data(self):
+        # TODO: implement display_data function
 
-def popular_month(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What is the most popular month for start time?
-    '''
-    # TODO: complete function
+    def calculate_statistics(self):
+        self.get_city()
+        self.get_time_period()
 
-
-def popular_day(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What is the most popular day of week (Monday, Tuesday, etc.) for start time?
-    '''
-    # TODO: complete function
-
-
-def popular_hour(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What is the most popular hour of day for start time?
-    '''
-    # TODO: complete function
-
-
-def trip_duration(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What is the total trip duration and average trip duration?
-    '''
-    # TODO: complete function
-
-
-def popular_stations(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What is the most popular start station and most popular end station?
-    '''
-    # TODO: complete function
-
-
-def popular_trip(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What is the most popular trip?
-    '''
-    # TODO: complete function
-
-
-def users(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What are the counts of each user type?
-    '''
-    # TODO: complete function
-
-
-def gender(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What are the counts of gender?
-    '''
-    # TODO: complete function
-
-
-def birth_years(city_file, time_period):
-    '''TODO: fill out docstring with description, arguments, and return values.
-    Question: What are the earliest (i.e. oldest user), most recent (i.e. youngest user),
-    and most popular birth years?
-    '''
-    # TODO: complete function
-
-
-def display_data():
-    '''Displays five lines of data if the user specifies that they would like to.
-    After displaying five lines, ask the user if they would like to see five more,
-    continuing asking until they say stop.
-
-    Args:
-        none.
-    Returns:
-        TODO: fill out return type and description (see get_city for an example)
-    '''
-    display = input('\nWould you like to view individual trip data?'
-                    'Type \'yes\' or \'no\'.\n')
-    # TODO: handle raw input and complete function
-
-
-def statistics():
-    '''Calculates and prints out the descriptive statistics about a city and time period
-    specified by the user via raw input.
-
-    Args:
-        none.
-    Returns:
-        none.
-    '''
-    # Filter by city (Chicago, New York, Washington)
-    city = get_city()
-
-    # Filter by time period (month, day, none)
-    time_period = get_time_period()
-
-    print('Calculating the first statistic...')
-
-    # What is the most popular month for start time?
-    if time_period == 'none':
+        print('Calculating the first statistic...')
         start_time = time.time()
-        
-        #TODO: call popular_month function and print the results
-        
+
+        if self.time_period == 'none':
+            # TODO: call popular_month function and print the results
+
+        if self.time_period == 'none' or self.time_period == 'month':
+            # TODO: call popular_day function and print the results
+
+        # TODO: call popular_hour function and print the results
+
+        # TODO: call trip_duration function and print the results
+
+        # TODO: call popular_stations function and print the results
+
+        # TODO: call popular_trip function and print the results
+
+        # TODO: call users function and print the results
+
+        # TODO: call gender function and print the results
+
+        # TODO: call birth_years function and print the results
+
         print("That took %s seconds." % (time.time() - start_time))
-        print("Calculating the next statistic...")
 
-    # What is the most popular day of week (Monday, Tuesday, etc.) for start time?
-    if time_period == 'none' or time_period == 'month':
-        start_time = time.time()
-        
-        # TODO: call popular_day function and print the results
-        
-        print("That took %s seconds." % (time.time() - start_time))
-        print("Calculating the next statistic...")    
+        self.display_data()
 
-    start_time = time.time()
-
-    # What is the most popular hour of day for start time?
-    # TODO: call popular_hour function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-    print("Calculating the next statistic...")
-    start_time = time.time()
-
-    # What is the total trip duration and average trip duration?
-    # TODO: call trip_duration function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-    print("Calculating the next statistic...")
-    start_time = time.time()
-
-    # What is the most popular start station and most popular end station?
-    # TODO: call popular_stations function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-    print("Calculating the next statistic...")
-    start_time = time.time()
-
-    # What is the most popular trip?
-    # TODO: call popular_trip function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-    print("Calculating the next statistic...")
-    start_time = time.time()
-
-    # What are the counts of each user type?
-    # TODO: call users function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-    print("Calculating the next statistic...")
-    start_time = time.time()
-
-    # What are the counts of gender?
-    # TODO: call gender function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-    print("Calculating the next statistic...")
-    start_time = time.time()
-
-    # What are the earliest (i.e. oldest user), most recent (i.e. youngest user), and
-    # most popular birth years?
-    # TODO: call birth_years function and print the results
-
-    print("That took %s seconds." % (time.time() - start_time))
-
-    # Display five lines of data at a time if user specifies that they would like to
-    display_data()
-
-    # Restart?
-    restart = input('\nWould you like to restart? Type \'yes\' or \'no\'.\n')
-    if restart.lower() == 'yes':
-        statistics()
-
+        restart = input('\nWould you like to restart? Type \'yes\' or \'no\'.\n')
+        if restart.lower() == 'yes':
+            self.calculate_statistics()
 
 if __name__ == "__main__":
-	statistics()
+    bike_share_stats = BikeShareStatistics()
+    bike_share_stats.calculate_statistics()
